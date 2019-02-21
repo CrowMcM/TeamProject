2173c8a594a2f9c72fc39edbde3a027b6901a19a
CrowMc2 <eoinmacmahon18@gmail.com>
2019-02-21 14:07:16 +0000

    updated gui

--- app/src/main/java/com/example/alcodeliveryapp/MainActivity.java
+++ app/src/main/java/com/example/alcodeliveryapp/MainActivity.java
@@ -1,13 +1,58 @@
 package com.example.alcodeliveryapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText Name;
+    private EditText Password;
+    private TextView Info;
+    private Button Login;
+    private int counter =3;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Name = (EditText)findViewById(R.id.etName);
+        Password = (EditText)findViewById(R.id.etPassword);
+        Info = (TextView)findViewById(R.id.tvInfo);
+        Login = (Button)findViewById(R.id.btnLogin);
+
+        Info.setText("Chances left: 3");
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                validate(Name.getText().toString(), Password.getText().toString());
+            }
+        });
+
     }
+
+    private void validate(String userName, String userPassword){
+        if((userName.equals("AlexP")) && (userPassword.equals("AlexH"))){
+            Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+            startActivity(intent);
+        }
+        else{
+            counter--;
+
+            Info.setText("Chances left: "+ String.valueOf(counter));
+
+            if(counter == 0){
+                Login.setEnabled(false);
+            }
+        }
+    }
+
 }
